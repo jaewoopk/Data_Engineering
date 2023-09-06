31e23ebb81018eff9809db7bbd33c66bfd5c27f7
jaewoopk <jaewoopk000@naver.com>
2023-09-06 16:47:07 +0900

    Day 1 To check out git works

--- src/main/java/kr/ac/sejong/kmook/Data_Engineering/App1.java
+++ src/main/java/kr/ac/sejong/kmook/Data_Engineering/App1.java
@@ -9,7 +9,7 @@ public class App1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("/Users/parkjaewoo/desktop/email-EuAll.txt"));
 
-        String line;
+        String line = null;
         while ((line = br.readLine()) != null) {
             if (line.startsWith("#")) {
                 continue;
