56f4ceb899fd56d08852243608a7c2dda2cd8b54
jaewoopk <jaewoopk000@naver.com>
2023-09-11 23:08:26 +0900

    Day 2 Trying to find max, min, to find ID with array, list, HashSet, TreeSet

--- src/main/java/kr/ac/sejong/kmook/Data_Engineering/App2.java
+++ src/main/java/kr/ac/sejong/kmook/Data_Engineering/App2.java
@@ -3,11 +3,18 @@ package kr.ac.sejong.kmook.Data_Engineering;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.TreeSet;
 
-public class App1 {
-    public static void main(String[] args) throws IOException {
+public class App2 {
+    public static void findMax(ArrayList<Email> email) {
+        TreeSet<Email> treeSet = new TreeSet<>(email);
+        System.out.println(treeSet.toString());
+    }
+    public static ArrayList<Email> init() throws IOException{
         BufferedReader br = new BufferedReader(new FileReader("/Users/parkjaewoo/desktop/email-EuAll.txt"));
+        ArrayList<Email> emailArrayList = new ArrayList<>();
 
         int lineCount = 0;
         String line = null;
@@ -17,8 +24,14 @@ public class App1 {
                 continue;
             }
             int[] emailToFrom = Arrays.stream(line.split("\t")).mapToInt(Integer::parseInt).toArray();
-            System.out.println(new Email(emailToFrom[0], emailToFrom[1]));
+            emailArrayList.add(new Email(emailToFrom[0], emailToFrom[1]));
         }
         System.out.println("lineCount => " + lineCount);
+        br.close();
+        return emailArrayList;
+    }
+    public static void main(String[] args) throws IOException {
+        ArrayList<Email> emailArrayList = init();
+        findMax(emailArrayList);
     }
 }
